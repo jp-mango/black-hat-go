c25dd141f650d8f8ccc66e60ac0245b12e8020ab
mango <menziesjustin@ymail.com>
2024-04-08 12:03:17 -0400

    - added TCP scanning funcs

--- main.go
+++ main.go
@@ -1,9 +1,45 @@
 package main
 
 import (
-	tcpscannersproxies "github.com/jp-mango/black-hat-go/02_tcp_scanners_proxies"
+	"fmt"
+	"net"
+	"sync"
 )
 
+func TCPscanner() string {
+	for i := 1; i <= 1024; i++ {
+		address := fmt.Sprintf("scanme.nmap.org:%d", i)
+		conn, err := net.Dial("tcp", address)
+		if err != nil {
+			// port is closed or filtered.
+			continue
+		}
+		conn.Close()
+		return fmt.Sprintf("%d open\n", i)
+	}
+	return ""
+}
+
+func FastTCPscanner() string {
+	var wg sync.WaitGroup
+	for i := 1; i <= 1024; i++ {
+		wg.Add(1)
+		go func(j int) {
+			defer wg.Done()
+			address := fmt.Sprintf("scanme.nmap.org:%d", j)
+			conn, err := net.Dial("tcp", address)
+			if err != nil {
+				return
+			}
+			conn.Close()
+			fmt.Printf("%d open\n", j)
+		}(i)
+	}
+	wg.Wait()
+	return ""
+}
+
 func main() {
-	tcpscannersproxies.FastTCPscanner()
+	result := FastTCPscanner()
+	fmt.Print(result)
 }
